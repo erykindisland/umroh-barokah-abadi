c648473a2e34a6caa00238d9ec730e1c36fac100
erykindisland <erykind.island@gmail.com>
2026-05-25 21:01:14 +0700

    wrangler

---
+++ .wrangler/tmp/pages-txeKo0/functionsRoutes-0.25618983000482165.mjs
@@ -0,0 +1,19 @@
+import { onRequest as __api_check_status_js_onRequest } from "D:\\GitHub\\umroh-barokah-abadi\\functions\\api\\check-status.js"
+import { onRequest as ___middleware_js_onRequest } from "D:\\GitHub\\umroh-barokah-abadi\\functions\\_middleware.js"
+
+export const routes = [
+    {
+      routePath: "/api/check-status",
+      mountPath: "/api",
+      method: "",
+      middlewares: [],
+      modules: [__api_check_status_js_onRequest],
+    },
+  {
+      routePath: "/",
+      mountPath: "/",
+      method: "",
+      middlewares: [___middleware_js_onRequest],
+      modules: [],
+    },
+  ]
